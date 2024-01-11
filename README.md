c9e126de7cc1dd1f270b92c2082dd10178cbab2b
okuyama <130431374+okuyama-code@users.noreply.github.com>
2024-01-12 05:51:41 +0900

    タブ切り替え完了

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ https://github.com/happiness-chain/practice/blob/main/typescript/school_user_lis
 
 ## TSクイズ
 https://github.com/happiness-chain/practice/blob/main/16_Typescript/001_typescript_quiz.md
+
+
+## TODO 仕様を細かく分解して攻略する
+ソート機能 -　生徒のみ表示の場合、勉強時間、ハピネススコアで昇順・降順で並び替えできる -　メンターのみ表示の場合、実務経験月数で昇順・降順で並び替えできる
+新規作成機能
+新規で生徒、メンターを登録することができる。（各ロールの項目を全て登録必須）
+リロードすると新規作成したユーザーは消えてOK
+
+
--- src/UserList.tsx
+++ src/UserList.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { USER_LIST } from './userListData'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { User } from './types';
 
 
 
-const UserList = () => {
+const UserList: React.FC<{}> = () => {
+  const studentList: User[] = USER_LIST.filter((user) => user.role === "student")
+
+  const mentorList: User[] = USER_LIST.filter((user) => user.role === "mentor")
+
+  
+
   return (
     <div>
       <div className='profile_tabs'>
@@ -107,10 +114,184 @@ const UserList = () => {
                   ))}
                 </TabPanel>
                 <TabPanel>
-                  生徒のみ
+                  {studentList.map((user) => (
+                    <div>
+                      <table className='table'>
+                        <tbody>
+                          <tr>
+                            <th>ID</th>
+                            <td>{user.id}</td>
+                          </tr>
+                          <tr>
+                            <th>名前</th>
+                            <td>{user.name}</td>
+                          </tr>
+                          <tr>
+                            <th>ロール</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>メールアドレス</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>年齢</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>郵便番号</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>電話番号</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>趣味</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>ホームページ</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>実務経験月数(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>勉強時間(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>課題番号(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>勉強中の言語(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>ハピネススコア(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>対応可能なメンター（課題番号が、担当範囲に含まれているメンターの名前を表示）</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>実務経験月数(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>現場で使っている言語(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>担当できる課題番号初め(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>担当できる課題番号終わり(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>対応可能な生徒（課担当範囲に含んでいる生徒の名前を表示）(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  ))}
                 </TabPanel>
                 <TabPanel>
-                  メンターのみ
+                  {mentorList.map((user) => (
+                    <div>
+                      <table className='table'>
+                        <tbody>
+                          <tr>
+                            <th>ID</th>
+                            <td>{user.id}</td>
+                          </tr>
+                          <tr>
+                            <th>名前</th>
+                            <td>{user.name}</td>
+                          </tr>
+                          <tr>
+                            <th>ロール</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>メールアドレス</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>年齢</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>郵便番号</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>電話番号</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>趣味</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>ホームページ</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>実務経験月数(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>勉強時間(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>課題番号(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>勉強中の言語(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>ハピネススコア(生徒のみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>対応可能なメンター（課題番号が、担当範囲に含まれているメンターの名前を表示）</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>実務経験月数(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>現場で使っている言語(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>担当できる課題番号初め(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>担当できる課題番号終わり(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                          <tr>
+                            <th>対応可能な生徒（課担当範囲に含んでいる生徒の名前を表示）(メンターのみ)</th>
+                            <td>{user.role}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  ))}
                 </TabPanel>
 
               </Tabs>
